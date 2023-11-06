90d623461130387f42263767f7cfa209680d6b0e
kybz1234 <kybz1234@gmail.com>
2023-11-05 20:15:54 -0600

    commit 2
    creacion de las vista para los usuarios,
    
    nota: faltan detalles por pulir

--- src/main/java/com/lenguajes/proyecto/controllers/basicController.java
+++ src/main/java/com/lenguajes/proyecto/controllers/basicController.java
@@ -5,12 +5,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping ("/home")
+@RequestMapping ("/")
 public class basicController {
 
-    @GetMapping(path = "/test")
-    public String saludar(){
+    @GetMapping(path = "/login")
+    public String login(){
         return "index";
+    }
+
+    @GetMapping(path = "/registro")
+    public String register(){
+        return "registro";
+    }
+
+     @GetMapping(path = "/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping(path = "/tienda")
+    public String shop(){
+        return "tienda";
+    }
+
+        @GetMapping(path = "/acerca")
+    public String about(){
+        return "acerca";
+    }
+
+            @GetMapping(path = "/contacto")
+    public String contact(){
+        return "contacto";
+    }
+
+                @GetMapping(path = "/producto")
+    public String item(){
+        return "producto";
     }
 
 }
